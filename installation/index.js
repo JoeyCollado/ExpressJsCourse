876e98d387df5e051f061592565dfb8e3930f205
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-05-06 16:18:31 +0800

    installed express and create a server

---
+++ installation/index.js
@@ -0,0 +1,33 @@
+import express from 'express' //after configuring package.json and adding type: "module"
+
+// app instance
+const app = express()
+
+//backend
+const PORT = 3000;
+
+//router define
+app.get('/', (request, response) => { //api endpoint, use req if we want to send any data in this api, use res if want to send any data from the backend
+    response.send('Hello ExpressJS2') // the '/' = represents root route, which means the response 'hello express' will appear in the root route
+}) 
+
+//initialize express app
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+}) //two parameter port number and callback function
+
+//script where if we make any changes it will restart our backend
+//npm i nodemon --save-dev
+
+//with this we created a basic server
+
+//to run it simply :
+//node nameofjsfile
+//node index.js
+//npm start = when we configure the package.json
+
+//to install express 
+//npm init -y
+//npm install express
+
+//also remember to always go to package.json and add our file.js in the scripts with start: ""
