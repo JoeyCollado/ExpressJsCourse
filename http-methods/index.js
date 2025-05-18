724db0dec47c537f6597e0df1422832075c35446
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-05-18 20:27:21 +0800

    http-methods

---
+++ http-methods/index.js
@@ -0,0 +1,59 @@
+//http methods = get, post, put, delete
+
+//GET = used to fetch data from server, parameters can be passed using router parameters or query strings
+//POST = used to send data to the server and create new resource, requires middleware (express.json()) to handle JSON input
+import express, { response } from 'express' //after configuring package.json and adding type: "module"
+import router from './route.js';
+
+// app instance
+const app = express()
+ 
+//backend
+const PORT = 3000;
+
+//router define
+app.get('/', (request, response) => { //api endpoint, use req if we want to send any data in this api, use res if want to send any data from the backend
+    response.send('Hello ExpressJS2') // the '/' = represents root route, which means the response 'hello express' will appear in the root route
+}) 
+
+// app.get('/user/login', userLogin)
+// app.get('/user/signup', userSignup)
+app.use('/user', router) //initialize route
+
+app.post('/users', express.json , (req,res) => { //simple post method, express.json = middleware
+    const {name, email} = req.body //destructuring
+    res.json({ //response
+        message: `User ${name} with email ${email} created successfully` 
+    })
+})
+
+//another route / about route
+app.get('/about', (request, response) => { 
+    response.send('This is about route')  //http://localhost:3000/about
+}) 
+
+//contact route
+app.get('/contact', (request, response) => { 
+    response.send('This is contact route')  //http://localhost:3000/contact
+})
+
+//initialize express app
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+}) //two parameter port number and callback function
+
+//script where if we make any changes it will restart our backend
+//npm i nodemon --save-dev
+
+//with this we created a basic server
+
+//to run it simply :
+//node nameofjsfile
+//node index.js
+//npm start = when we configure the package.json
+
+//to install express 
+//npm init -y
+//npm install express
+
+//also remember to always go to package.json and add our file.js in the scripts with start: ""
