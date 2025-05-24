dc2d05e8ab2bab394f56cda62a6ac89a5e49d874
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-05-24 13:47:23 +0800

    Sending a message to the console using middlewarE

--- middleware/index.js
+++ middleware/index.js
@@ -7,20 +7,31 @@ const app = express()
 //backend
 const PORT = 3000;
 
+app.use((req,res, next) => {
+    console.log('Start')
+    next()
+})
+
+/*
 app.use('/welcome', (req, res, next) => { //application level middleware, applies to all route
     console.log(`A new request receieved at` + Date.now())
     next(); //middleware
 }) //go to localhost 3000 to check
+*/
 
 //router define
 //get request
 app.get('/', (request, response) => { //api endpoint, use req if we want to send any data in this api, use res if want to send any data from the backend
-    response.send('Hello ExpressJS2') // the '/' = represents root route, which means the response 'hello express' will appear in the root route
+    console.log('Middle')
+    response.send('Hello Express') // the '/' = represents root route, which means the response 'hello express' will appear in the root route
 }) 
 
+/*
 app.get('/welcome', (request, response) => { //by adding the route in the middleware,that middleware will only apply to this route
     response.send('Welcome to express app') 
 }) 
+*/
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
