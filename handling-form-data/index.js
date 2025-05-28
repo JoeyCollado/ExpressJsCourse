e807a92da4225c8f36de57570501bcce06e7b6bf
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-05-28 19:46:15 +0800

    handling-form-data

---
+++ handling-form-data/index.js
@@ -0,0 +1,29 @@
+import express, { response } from 'express' //after configuring package.json and adding type: "module"
+// app instance
+const app = express()
+//backend
+const PORT = 3000;
+
+
+app.get('/', (request, response) => { 
+    response.send('Hello Express')
+}) 
+
+//get txt file from backend server
+app.use('/public', express.static('public'))
+app.use('/images', express.static('images')) //exposing images folder and accessing its files
+
+//go to http://localhost:3000/example.txt to check response of file
+//another example = http://localhost:3000/jek2.png for image
+
+//after exposing as public routes
+//this is how you access it http://localhost:3000/images/jek2.png
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
+
+
+
+//handling form data
