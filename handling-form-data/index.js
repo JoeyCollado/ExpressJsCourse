750c7af2e1853d63ab981ed9278a672b7ebb310a
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-05-29 11:52:26 +0800

    sending normal form data and sending file/image using form data

--- handling-form-data/index.js
+++ handling-form-data/index.js
@@ -1,10 +1,15 @@
 import express, { response } from 'express' //after configuring package.json and adding type: "module"
+import multer from 'multer';
 // app instance
 const app = express()
 //backend
 const PORT = 3000;
+//
+const upload = multer(); //instance
 
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extended:true})) //form url encoded
+//
+app.use(upload.single('image')) //normal form data
 
 
 app.get('/', (request, response) => { 
@@ -14,6 +19,7 @@ app.get('/', (request, response) => {
 //route
 app.post('/form', (req,res) => {
     console.log(req.body)
+    console.log(req.file)
     res.send('Form received')
 })
 
@@ -34,3 +40,6 @@ app.listen(PORT, () => {
 //sending normal form data
 //to use it we need external package
 //npm i multer
+//same process as url encoded form data
+
+//sending file in multi-art form data
