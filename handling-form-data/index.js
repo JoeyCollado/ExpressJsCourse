6f98b10ad7051f8d88190689e8c9aa680872bb74
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-06-01 10:58:22 +0800

    sending file in their original filename, either png or jpeg for image

--- handling-form-data/index.js
+++ handling-form-data/index.js
@@ -1,44 +1,42 @@
-import express, { response } from 'express' //after configuring package.json and adding type: "module"
-import multer from 'multer';
+import express, { response } from "express"; //after configuring package.json and adding type: "module"
+import multer from "multer";
 
 //
 const storage = multer.diskStorage({
-    destination:'uploads'
-})
+  destination: "uploads",
+  filename: (req, file, cb) => {
+    cb(null,file.fieldname + "_" + Date.now() + file.originalname);
+  },
+});
 
 // app instance
-const app = express()
+const app = express();
 //backend
 const PORT = 3000;
 //
-const upload = multer({storage}); //instance
+const upload = multer({ storage }); //instance
 //
 
-app.use(express.urlencoded({extended:true})) //form url encoded
+app.use(express.urlencoded({ extended: true })); //form url encoded
 //
-app.use(upload.single('image')) //normal form data
-
+app.use(upload.single("image")); //normal form data
 
 //disk storage configuration
 
-
-
-app.get('/', (request, response) => { 
-    response.send('Hello Express')
-}) 
+app.get("/", (request, response) => {
+  response.send("Hello Express");
+});
 
 //route
-app.post('/form', (req,res) => {
-    console.log(req.body)
-    console.log(req.file)
-    res.send('Form received')
-})
+app.post("/form", (req, res) => {
+  console.log(req.body);
+  console.log(req.file);
+  res.send("Form received");
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-})
-
-
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
 
 //handling form data
 //postman
