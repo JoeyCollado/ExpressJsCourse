aef358b75fdbbfdc466c09b5c7fc150d5580ccb4
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-06-10 16:37:18 +0800

    applying middleware to all routes

--- connecting-to-database/index.js
+++ connecting-to-database/index.js
@@ -9,12 +9,16 @@ const PORT = 3000;
 //importing mongodb connection logic
 await connectDB();
 
+app.use(express.json())
+
 app.get("/", (req, res) => {
   res.send("Hello Express");
 });
 
 //routes
-app.post('/person', express.json(), async (req,res) => {
+
+//post method
+app.post('/person',  async (req,res) => {
   const {email, name, age} = req.body
   const newPerson = new Person({
     name,
@@ -25,6 +29,21 @@ app.post('/person', express.json(), async (req,res) => {
   console.log(newPerson)
   res.send("Person added")
 })
+
+//update method
+app.put('/person',  async (req,res) => {
+  const {email, name, age} = req.body
+  const newPerson = new Person({
+    name,
+    age,
+    email
+  })
+  await newPerson.save()
+  console.log(newPerson)
+  res.send("Person added")
+})
+
+//saving data method
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
