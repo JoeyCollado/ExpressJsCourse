ee3418bf497e6ccade77d60f6a366af85451fda7
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-06-16 16:33:33 +0800

    making the object empty but still will show without data

--- connecting-to-database/models/Person.js
+++ connecting-to-database/models/Person.js
@@ -5,8 +5,8 @@ const personSchema = new mongoose.Schema({
     name: {type: String, required: true},
     age: {type: Number, required: true},
     email: {type: String, required: true, unique:true}, 
-    userOrder: {type:Object, default:{test: "test value"}}
-}, {timestamps:true})
+    userOrder: {type:Object, default:{}}
+}, {timestamps:true, minimize:false})
 
 //create model
 export const Person = mongoose.model("Person", personSchema)
