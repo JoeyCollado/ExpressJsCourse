d200ffe5c95064a6e725cce47e5abcabc23a028e
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-06-16 16:30:37 +0800

    object default value

--- connecting-to-database/models/Person.js
+++ connecting-to-database/models/Person.js
@@ -4,7 +4,8 @@ import mongoose from "mongoose";
 const personSchema = new mongoose.Schema({
     name: {type: String, required: true},
     age: {type: Number, required: true},
-    email: {type: String, required: true, unique:true},
+    email: {type: String, required: true, unique:true}, 
+    userOrder: {type:Object, default:{test: "test value"}}
 }, {timestamps:true})
 
 //create model
