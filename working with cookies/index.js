2ee11f85dc2314f1f7bce3a1a81ca787ee854094
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-06-28 16:42:57 +0800

    installing cookie parser package and using it as middleware

--- working with cookies/index.js
+++ working with cookies/index.js
@@ -1,10 +1,12 @@
 import express from "express"; //after configuring package.json and adding type: "module"
-
+import cookieParser from "cookie-parser"; //importing cookie-parser
 
 // app instance
 const app = express();
 //backend
 const PORT = 3000;
+//adding cookie parser as middleware
+app.user(cookieParser())
 
 app.get("/", (req, res) => {
   res.send("Hello Express");
@@ -24,3 +26,5 @@ app.listen(PORT, () => {
 //personalization   - enhancing user experience with recommendations
 //user tracking     - monitoring user behaviour on the website
 
+//npmjs.com - website
+//npm i cookie-parser - package
