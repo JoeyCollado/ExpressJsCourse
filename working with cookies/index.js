9207b0d4d1a82946aa53e90bb070627b2357e271
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-06-28 16:39:54 +0800

    Setting up

--- working with cookies/index.js
+++ working with cookies/index.js
@@ -1,78 +1,26 @@
-import express, { response } from "express"; //after configuring package.json and adding type: "module"
-import { connectDB } from "./config/db.js";
-import { Person } from "./models/Person.js";
+import express from "express"; //after configuring package.json and adding type: "module"
+
 
 // app instance
 const app = express();
 //backend
 const PORT = 3000;
-//importing mongodb connection logic
-await connectDB();
-
-app.use(express.json())
 
 app.get("/", (req, res) => {
   res.send("Hello Express");
 });
 
-//routes
 
-//post method
-app.post('/person',  async (req,res) => {
-  const {email, name, age} = req.body
-  const newPerson = new Person({
-    name,
-    age,
-    email
-  })
-  await newPerson.save()
-  console.log(newPerson)
-  res.send("Person added")
-})
-
-//saving data in mongodb
-app.post('/person', async (req,res) => {
-  try{
-    const {email, name, age} = req.body
-    const newPerson = new Person({
-      name,
-      age,
-      email
-    })
-    await newPerson.save()
-    console.log(newPerson)
-    res.send("Person added")
-  }catch(error){
-    res.send(error.message)
-  }
-})
-
-//update method
-app.put('/person',  async (req,res) => {
-  const {id} = req.body
-  //find data using email id
-  const personData = await Person.findByIdAndUpdate(id, {age:'28'})
-  
-  console.log(personData)
-  res.send("Person updated")
-})
-
-//saving data method
-
-//delete method
-app.delete('/person/:id', async (req,res) => {
-   const {id} = req.params
-   await Person.findByIdAndDelete(id)
-   res.send('User Deleted')
-})
-
-//
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-//mongodb
-//using mongodbatlas service to create a mongodb database
+//cookies = are small data files sent from the server to the client, stored on the client browser.
+//          they are sent back with every request to the server
 
-//creating a schema and a model to perform crud operations in mongodb
+//use of cookies 
+//sesion management - maintaining user login session
+//personalization   - enhancing user experience with recommendations
+//user tracking     - monitoring user behaviour on the website
+
