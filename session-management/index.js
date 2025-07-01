962e129fda8a1c9412ab30070db239287f15495a
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-07-01 14:58:56 +0800

    Basic page view session

--- session-management/index.js
+++ session-management/index.js
@@ -1,5 +1,6 @@
 import express from "express"; //after configuring package.json and adding type: "module"
 import cookieParser from "cookie-parser"; //importing cookie-parser
+import session from "express-session";
 
 // app instance
 const app = express();
@@ -7,10 +8,24 @@ const app = express();
 const PORT = 3000;
 //adding cookie parser as middleware
 app.use(cookieParser())
+//set up session as middleware
+app.use(session({
+  secret: 'sample-secret',
+  resave: false, //session will not be save again and again once we apply changes
+  saveUninitialized: false, //prevent empty session to be stored
+}))
 
 app.get("/", (req, res) => {
   res.send("Hello Express");
 });
+
+//route
+//basic page view session
+app.get('/visit', (req,res) => {
+  if(req.session.page_views) {
+    
+  } //checks if page views property is available
+})
 
 
 
