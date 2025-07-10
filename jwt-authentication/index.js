0f56ad4b02d2d0b32dde11a0a08e67f2aabf62ff
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-07-10 20:17:01 +0800

    sending jwt as response

--- jwt-authentication/index.js
+++ jwt-authentication/index.js
@@ -1,5 +1,7 @@
-import express from "express"; //after configuring package.json and adding type: "module"
+import express, { response } from "express"; //after configuring package.json and adding type: "module"
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
 // app instance
 const app = express();
 //backend
@@ -23,10 +25,12 @@ app.post('/register', async (req,res) => { //everytime we access this api we wil
 app.post('/login', async (req,res) => { //everytime we access this api we will get user name and password and store properties in array
   const {username, password} = req.body;
   const user = users.find(u => u.username === username) //verification
-  if(!user || password !== user.password){ //condition to check if credentials are matched
+  if(!user || !(await bcrypt.compare(password, user.password))){ //condition to check if credentials are matched
       return res.send('Not Authorized')
   }// if correct create session
- 
+  //send token to the response
+  const token = jwt.sign({username}, 'test#secret') //data, secretKey
+  res.json({token})
 })
 
 //
