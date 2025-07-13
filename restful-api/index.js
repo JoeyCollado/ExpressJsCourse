e47cd0d27f5e526c95fd56ca799521972ca91c85
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-07-13 21:15:28 +0800

    creating api endpoint for product

--- restful-api/index.js
+++ restful-api/index.js
@@ -1,6 +1,5 @@
 import express, { response } from "express"; //after configuring package.json and adding type: "module"
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
+
 
 // app instance
 const app = express();
@@ -10,54 +9,23 @@ const PORT = 3000;
 
 app.use(express.json());
 
-const users = []; //array
+app.get('/', (req,res) => {
+  res.send("hello express");
+})
 
-app.post("/register", async (req, res) => {
-  //everytime we access this api we will get user name and password and store properties in array
-  const { username, password } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10); //hash the password and store password in array, 10 refers to number of rounds
-  users.push({
-    //registering objects two array
-    username,
-    password: hashedPassword,
-  });
-  res.send("User Registered");
-});
+//api endpoints, sending response using rest principles
 
-app.post("/login", async (req, res) => {
-  //everytime we access this api we will get user name and password and store properties in array
-  const { username, password } = req.body;
-  const user = users.find((u) => u.username === username); //verification
-  if (!user || !(await bcrypt.compare(password, user.password))) {
-    //condition to check if credentials are matched
-    return res.send("Not Authorized");
-  } // if correct create session
-  //send token to the response
-  const token = jwt.sign({ username }, "test#secret"); //data, secretKey
-  res.json({ token });
-});
+//GET all product
+app.get('/api/products', (req,res) => {
+  const products = [ //array
+    {id:1, name:'Laptop', price:1000}, //object
+    {id:2, name:'Phone', price:500}
+  ]
 
-//
-app.get("/dashboard", (req, res) => {
-  try {
-    //can only be accessed by user who is logged in the server
-    //adding verification method so that if its correct we can access dashboard route
-    //get token from the header
-    const token = req.header("Authorization");
-    //send token on server
-    //verify token
-    const decodedToken = jwt.verify(token, "test#secret");
-    //check if username is available
-    if (decodedToken.username) {
-      res.send(`Welcome ${decodedToken.username}`);
-    } // if incorrect
-    else {
-      res.send("Access Denied");
-    }
-  } catch (error) {
-    res.send("Access Denied");
-  }
-});
+  res.status(200).json({products}) //response status = request was executed successfully, and sending it in json format
+})
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
