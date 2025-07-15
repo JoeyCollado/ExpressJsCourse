eb89cc86d2a803e3a561dfecd96c4bd102c9f276
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-07-15 22:11:34 +0800

    done

--- project-scaffolding/index.js
+++ project-scaffolding/index.js
@@ -12,45 +12,17 @@ app.get("/", (req, res) => {
   res.send("hello express");
 });
 
-//api endpoints, sending response using rest principles
 
-//GET all product
-app.get("/api/products", (req, res) => {
-  const products = [
-    //array
-    { id: 1, name: "Laptop", price: 1000 }, //object
-    { id: 2, name: "Phone", price: 500 },
-  ];
-  res.status(200).json({ products }); //response status = request was executed successfully, and sending it in json format
-});
-
-//API to get single product
-app.get("/api/products/:id", (req, res) => {
-  const products = [
-    //array
-    { id: 1, name: "Laptop", price: 1000 }, //object
-    { id: 2, name: "Phone", price: 500 },
-  ];
-  //finding product data by id and sending it as response
-  const product = products.find((p) => p.id === Number(req.params.id)); //Number converts string to number
-
-  //conditional checking
-  if (!product) {
-    return res.status(404).json({ message: "Product not Found" }); //status code if data is not available
-  }
-  //if product is available
-  res.status(200).json(product); //product is already an object so using the product name is enough for the constructor
-});
-
-//API to Create a new product
-app.post('/api/products', (req,res) => {
-  const newProduct = req.body
-  newProduct.id = Date.now()
-  res.status(201).json(newProduct)
-})
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 //npm i express-generator
+
+//to create an express app
+//use the npm package
+//do the command
+//open the folder with vscode
+//instal npm package with = npm install
+//you're set to go
